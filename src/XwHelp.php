e3315e4af362e1705d398ada72b0a73d09bcb300
xwleo <liuxudong980@gmail.com>
2023-12-28 14:21:01 +0800

    v1.2.1 數組轉json

--- src/XwHelp.php
+++ src/XwHelp.php
@@ -21,18 +21,18 @@ class XwHelp
     }
 
     //模擬處理data轉json
-    public static function dataToJson($data, $isJson = true)
+    public static function msgToJson($message, $isJson = true)
     {
+        $trace = debug_backtrace();
+        $caller = $trace[0];
         $data = [
-            'code' => !empty($data['code']) ? $data['code'] : 0,
-            'message' => !empty($data['message']) ? $data['message'] : '',
-            'file' => !empty($data['file']) ? $data['file'] : '',
-            'line' => !empty($data['line']) ? $data['line'] : 0,
+            'message' => !empty($message) ? $message : '',
+            'file' => !empty($caller['file'])? $caller['file'] : '',
+            'line' => !empty($caller['line'])? $caller['line'] : '',
         ];
         if ($isJson) {
             return json_encode($data);
         }
-
         return $data;
     }
 }
